5f8512bc941ce21880796fffdbc845df31ba267d
jessekathryn <jessekathrynbarnes@gmail.com>
2020-04-28 17:09:28 -0400

    models updated w/updated methods, serializers created

--- backend/app/models/profile.rb
+++ backend/app/models/profile.rb
@@ -1,4 +1,36 @@
 class Profile < ApplicationRecord
     has_many :activities
     validates :name, :balance, presence: true
+
+
+
+def update_balance(activity)
+    if activity.type == 'deposit'
+        self.balance = self.balance + activity.amount
+        self.save
+    elsif activity.type == 'debit'
+        if self.balance >= activity.amount
+          self.balance = self.balance - activity.amount
+          self.save
+        else
+          return 'Balance too low.'
+        end
+      end
+    end
+
+def update_balance_on_delete(activity)
+    if activity.type == 'deposit'
+        self.balance >= activity.amount
+        self.balance = self.balance - activity.amount
+        self.save
+        else
+            return 'Balance too low'
+        end
+    elsif
+        activity.type == 'debit' 
+        self.balance = self.balance + activity.amount
+        self.save
+    end
+end
+    
 end
