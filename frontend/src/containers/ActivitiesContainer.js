386703eb788747d6af4da79a7483bdece20fffc4
jessekathryn <jessekathrynbarnes@gmail.com>
2020-04-28 19:27:44 -0400

    added container for profiles/container for Activities

---
+++ frontend/src/containers/ActivitiesContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ActivityInput from '../components/ActivityInput'
+import Activities from '../components/Activities'
+
+class ActivitiesContainer extends React.Component {
+
+    render() {
+        return (
+          <div>
+              <ActivityInput profile={this.props.profile}/><br/>
+              <Activities activities={this.props.profile && this.props.profile.activities}/>
+          </div>
+        )
+      }
+    }
+
+export default ActivitiesContainer
