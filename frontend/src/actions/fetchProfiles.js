4d7efad8988c6a713d8c3be37ba176a9f611a062
jessekathryn <jessekathrynbarnes@gmail.com>
2020-04-28 18:28:21 -0400

    fetchProfiles completed

---
+++ frontend/src/actions/fetchProfiles.js
@@ -0,0 +1,10 @@
+export function fetchProfiles() {
+    return (dispatch) => {
+      fetch('http://localhost:3000/api/v1/profiles')
+      .then(resp => resp.json())
+      .then(profiles => dispatch({
+        type: 'FETCH_PROFILES',
+        payload: profiles
+      }))
+    }
+  }
