260c177b0f565c0a2a3db72272dfeb039f933396
jessekathryn <jessekathrynbarnes@gmail.com>
2020-04-28 19:24:48 -0400

    added container for profiles/container for Activities

---
+++ frontend/src/containers/ProfilesContainer.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Route, Switch} from 'react-router-dom'
+import {fetchProfiles} from '../actions/fetchProfiles'
+import Profiles from '../components/Profiles'
+import Profile from '../components/Profile'
+import ProfileInput from '../components/ProfileInput'
+import NavBar from '../components/NavBar'
+
+class ProfilesContainer extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchProfiles()
+  }
+
+  render() {
+    return (
+        <div>
+          <NavBar/>
+          <Switch>
+            <Route path='/profiles/new' component={ProfileInput}/>
+            <Route path='/profiles/:id' render={(routerProps) => <Profile {...routerProps} profiles={this.props.profiles}/>}/>
+            <Route path='/profiles' render={(routerProps) => <Profiles {...routerProps} profiles={this.props.profiles}/>}/>
+          </Switch>
+        </div>
+    )
+}
+}
+
+const mapStateToProps = state => {
+    return {
+      profiles: state.profiles
+    }
+  }
+  
+export default connect(mapStateToProps, {fetchProfiles})(ProfilesContainer)
+  
