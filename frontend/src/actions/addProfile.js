116286653bc7beb1bbacb0d0d7c818b475a75fe5
jessekathryn <jessekathrynbarnes@gmail.com>
2020-04-28 18:18:14 -0400

    completed actions: addActivity, addProfile

---
+++ frontend/src/actions/addProfile.js
@@ -0,0 +1,14 @@
+return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/profiles', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(account => dispatch({type: 'ADD_PROFILE', payload: profile}))
+  }
+
+}
