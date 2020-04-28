d940f73ecbddd603ace32a9fc296ec1463d1b556
jessekathryn <jessekathrynbarnes@gmail.com>
2020-04-28 18:26:17 -0400

    editProfile completed

---
+++ frontend/src/actions/editProfile.js
@@ -0,0 +1,15 @@
+export const editProfile = (data) => {
+    return (dispatch) => {
+      fetch(`http://localhost:3000/api/v1/profiles/${data.id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        method: 'PATCH',
+        body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+      .then(profile => dispatch({type: 'EDIT_PROFILE', payload: profile}))
+    }
+  
+  }
