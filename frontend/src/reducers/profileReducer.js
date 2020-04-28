01b932db26701ead2448f6060549cbaab5f6b7ff
jessekathryn <jessekathrynbarnes@gmail.com>
2020-04-28 19:29:43 -0400

    reducer - profile reducer created

---
+++ frontend/src/reducers/profileReducer.js
@@ -0,0 +1,14 @@
+export default function accountReducer(state = {accounts: []}, action) {
+    switch (action.type) {
+      case 'FETCH_ACCOUNTS':
+        return {accounts: action.payload}
+      case 'ADD_ACCOUNT':
+        return {...state, accounts: [...state.accounts, action.payload]}
+      case 'ADD_TRANSACTION':
+        let accounts = state.accounts.map(account => {
+          if (account.id === action.payload.id) {
+            return action.payload
+          } else {
+            return account
+          }
+        })
