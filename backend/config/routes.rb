98a4e9ec436d378e117d1f9bfa418c8f0e583a3a
jessekathryn <jessekathrynbarnes@gmail.com>
2020-05-04 21:49:00 -0400

    frontend desing

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
     namespace :api do
       namespace :v1 do
         resources :profiles do
@@ -7,5 +6,4 @@ Rails.application.routes.draw do
         end
       end
     end
-  
   end
